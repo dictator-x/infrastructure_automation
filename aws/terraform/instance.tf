028f1a400970c32430ca54f815c963bd2daf7bb7
dictator-x <dictator.xwu@gmail.com>
2020-08-19 11:04:57 -0400

    use file credential

--- aws/terraform/instance.tf
+++ aws/terraform/instance.tf
@@ -1,16 +1,4 @@
-provider "aws" {
-  access_key = "${var.AWS_ACCESS_KEY}"
-  secret_key = "${var.AWS_SECRET_KEY}"
-  region = "${var.AWS_REGION}"
-}
-
-# provider "aws" {
-#   region = "${var.AWS_REGION}"
-#   shared_credentials_file = "/home/dictator/.aws"
-#   profile = "default"
-# }
-
 resource "aws_instance" "example" {
-  ami = "ami-02354e95b39ca8dec"
+  ami = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
 }
